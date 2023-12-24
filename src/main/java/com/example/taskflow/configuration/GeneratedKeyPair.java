b4cb8502deec483fb0837c12b259f6e1deba671d
moussafia <moussafia.mohammed1999@gmail.com>
2023-12-24 20:23:56 +0100

    generated key pair for rsa

---
+++ src/main/java/com/example/taskflow/configuration/GeneratedKeyPair.java
@@ -0,0 +1,28 @@
+package com.example.taskflow.configuration;
+
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class GeneratedKeyPair {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        var keyPair = keyPairGenerator.generateKeyPair();
+        byte[] publicKey = keyPair.getPublic().getEncoded();
+        byte[] privateKey = keyPair.getPrivate().getEncoded();
+        PemWriter pemWriterPub = new PemWriter(new OutputStreamWriter(new FileOutputStream("src/main/resources/keys/public.pem")));
+        PemObject pemObjectPub = new PemObject("PUBLIC KEY", publicKey);
+        pemWriterPub.writeObject(pemObjectPub);
+        pemWriterPub.close();
+        PemWriter pemWriterPri = new PemWriter(new OutputStreamWriter(new FileOutputStream("src/main/resources/keys/private.pem")));
+        PemObject pemObjectPri = new PemObject("PRIVATE KEY", privateKey);
+        pemWriterPri.writeObject(pemObjectPri);
+        pemWriterPri.close();
+    }
+}
